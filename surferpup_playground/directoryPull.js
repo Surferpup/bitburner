e36c2266a9aa2082f7e175d2ecc7fdce9d73a6ea
Surferpup <mike.simpkins@me.com>
2023-04-07 15:25:07 -0700

    Update directoryPull.js

--- surferpup_playground/directoryPull.js
+++ surferpup_playground/directoryPull.js
@@ -1,22 +1,22 @@
 /** @param {NS} ns */
 // File to keep global values for all scripts
-export const DIRS = {HIVE : "hive", HACKS: "hacks", STOCKS : "stocks", UTILS : "utils"}
+export const DIRS = { HIVE: "hive", HACKS: "hacks", STOCKS: "stocks", UTILS: "utils" }
 
 export async function main(ns) {
 	switch (ns.args[0]) {
 		case "export":
-			for( const property in DIRS ) {
+			for (const property in DIRS) {
 				ns.tprintf(`${property}: ${DIRS[property]}`)
 			} // end for
-			getFileList(ns,"home",ns.args[1],ns.args[2]);
+			getFileList(ns, "home", ns.args[1], ns.args[2]);
 		// end case
 	} // end switch
 }// end function main(ns)
 
-export function getFileList(ns,server,dir,root_only) {
-	let directory_list = ns.ls(server,dir)
+export function getFileList(ns, server, dir, root_only) {
+	let directory_list = ns.ls(server, dir)
 	for (const filename of directory_list) {
-		if ((root_only)  && (filename.startsWith("/"))) {
+		if ((root_only) && (filename.startsWith("/"))) {
 			ns.tprintf(`SKIPPED ${filename}`)
 			continue;
 		} // end if
