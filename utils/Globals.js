8323963a3e9170940660d859848724f04b1e0763
Surferpup <mike.simpkins@me.com>
2023-04-11 02:39:28 -0700

    Fixed Globals.js

--- utils/Globals.js
+++ utils/Globals.js
@@ -1,52 +1,78 @@
-/** @param { NS } ns **/
-// File to keep global values for all scripts
-export const DIRS = { HIVE: "hive", HACKS: "hacks", STOCKS: "stocks", UTILS: "utils" }
-
-/** @param { NS } ns */
-export async function main(ns) {
-	switch (ns.args[0]) {
-		case "export":
-			
-			for (const filename of getFileList(ns, ns.args[1], ns.args[2], ns.args[3], false)) {
-				ns.tprintf(filename)
-                
-			} //end for
-			
-		// end case
-	} // end switch
-}// end function main(ns)
-
-/* function getFileList(ns, server, grep, root_only, debug)
-
-	This function returns a list of copyable files from a server.
-	Copyable files include .js and .txt files only.
-	
-	Requires: 	server    	-- 	the name of the server to run thef
-								directory command
-	
-	Optional: 	grep      	--	what to grep for ("." is everything)
-				root_only 	--	use true if only want root level files
-				debug		--	use true only if you want terminal output for debugging
+/** File to keep global values for all scripts */
+/**
+ * @const { obj } DIRS - Default Directory paths for scripts
+ * @property { string } HIVE - Location of hive scripts
+ * @property { string } HACKS - Location of hacking scripts
+ * @property { string } STOCKS - Location of stock scripts
+ * @property { string } UTILS - Location of utility scripts (including this file)
 */
-/** @param { NS } ns **/
-export function getFileList(ns, server = "home", grep = ".", root_only, debug = false) {
-	let filelist = []
-	//if ( ! server ) { server = "home" }
-	//if ( ! grep ) { grep = "." }
-	let directory_list = ns.ls(server, grep)
-	for (const filename of directory_list) {
-		if (((root_only) && (filename.startsWith("/"))) ||
-			((!filename.includes(".js")) && (!filename.includes(".txt")))) {
-			if (debug) ns.tprintf(`SKIPPED ${filename}`)
-			continue;
-		} // end if
-		if (filename.startsWith("/")) {
-			if (debug) ns.tprintf(filename.slice(1))
-			filelist.push(filename.slice(1))
-		} else {
-			if (debug) ns.tprintf(filename)
-			filelist.push(filename)
-		} // end if .. else
-	} // end for
-	return filelist
+export const DIRS = { HIVE: "hive", HACKS: "hacks", STOCKS: "stocks", UTILS: "utils" };
+/**
+ * Driver function to access some of the global functions
+ * @param { NS } ns Netscript namespace
+ * @remarks
+ * Can take up to five arguments, all of which are optional.
+ * The first argument is a useage arguement (--help, etc)
+ *
+ * @see getFileList for details
+ **/
+export async function main(ns) {
+    const switchCase = (ns.args[0]) ? ns.args[0] : "export";
+    switch (switchCase) {
+        case "export":
+            const filelist = getFileList(ns, ns.args[1], ns.args[2], ns.args[3], ns.args[4])
+            for (const filename of filelist) {
+                ns.tprintf(filename);
+            } //end for
+            ns.tprintf(`------------------\nPrinted ${filelist.length}`)
+        // end case
+    } // end switch
+} // end function main(ns)
+/**
+ * Returns a directory listing of files on a server (only *.js and *.txt files are returned)
+ * @param { NS } ns  Netscript Namespace
+ * @param { string } [server="home"] Name of server (default = "home" )
+ * @param { string } [grep]  Search string (default = "." for all files)
+ * @param { boolean } [root_only] Only return files in root directory (default = false)
+ * @param { boolean } [debug] Prints to terminal result of every file for debugging (default = false)
+ * @returns { string[]  } A list of file names
+ **/
+export function getFileList(ns, server = "home", grep = ".", root_only = false, debug = false) {
+    let filelist = [];
+    if (!server) {
+        server = "home";
+    }
+    if (!grep) {
+        grep = ".";
+    }
+    let directory_list = ns.ls(server, grep);
+    for (const filename of directory_list) {
+        if (((root_only) && (filename.startsWith("/"))) ||
+            ((! (filename.includes(".js"))) && (!(filename.includes(".txt"))))) {
+            if (debug) {
+                ns.tprintf(`SKIPPED ${filename}`);
+            } //end if
+
+            continue // skip this file
+        } // end if
+        if (filename.startsWith("/")) {
+            filelist.push(filename.slice(1));
+
+            //check debug
+            if (debug) {
+                ns.tprintf(filename.slice(1));
+            } //end if
+            
+        } else {
+            filelist.push(filename);
+            
+            // check debug
+            if (debug) {
+                ns.tprintf(filename);
+            } //end if
+            
+        } // end if .. else
+    } // end for
+    return filelist;
 } // end function getFileList
+//end file Globals.js
