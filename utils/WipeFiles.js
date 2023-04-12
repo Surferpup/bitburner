69dc2d27a553f99d370cf050071e241c943ce658
Surferpup <mike.simpkins@me.com>
2023-04-11 19:17:23 -0700

    Added Exclusions to wipe

--- utils/WipeFiles.js
+++ utils/WipeFiles.js
@@ -8,14 +8,16 @@ import { getFileList } from "/utils/Globals"
 /** WIll wipe all files fom home server */
 /** @param { NS } ns **/
 export async function main(ns) {
-    const result =  ns.prompt("This will wipe ALL files from the server (except for this utility)\n -- are you sure you want to do this?")
+    const result =  await ns.prompt("This will wipe ALL files from the server (except for this utility)\n -- are you sure you want to do this?")
+
+    const exclusions = ["utils/wget.js"]
 
     if (result) {
         
         var filelist = getFileList(ns,null,".",null,false)
         ns.tprintf(`Started with ${filelist.length} Files.`)
         for (let filename of filelist) {
-            if (ns.rm("/" + filename))
+            if (( exclusions.indexOf(filename) >=0 ) && ns.rm("/" + filename))
                 ns.tprintf(`Removed ${filename}`)
             else
                 ns.tprintf(`SKIPPED ${filename}`)
